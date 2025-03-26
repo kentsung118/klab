e1643941b6b348adb8ba81bf3784a94018ea46e3
kentsung <kentsung@17.media>
2025-03-26 16:22:06 +0800

    Onclick solution2

--- app/src/main/java/com/kent/lab/touchevent/ViewGroup2.kt
+++ app/src/main/java/com/kent/lab/touchevent/ViewGroup2.kt
@@ -31,14 +31,14 @@ class ViewGroup2 : LinearLayout {
     override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
         Log.i(TAG, "onInterceptTouchEvent action:" + StringUtils.getMotionEventName(event))
 
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                downX = event.x
-                downY = event.y
-                isClicking = true
-                Log.d(TAG, "onInterceptTouchEvent action:" + StringUtils.getMotionEventName(event) + " " + true)
-                return true // 攔截 DOWN，防止子 View 消費
-            }
+//        when (event.action) {
+//            MotionEvent.ACTION_DOWN -> {
+//                downX = event.x
+//                downY = event.y
+//                isClicking = true
+//                Log.d(TAG, "onInterceptTouchEvent action:" + StringUtils.getMotionEventName(event) + " " + true)
+//                return true // 攔截 DOWN，防止子 View 消費
+//            }
 
 //            MotionEvent.ACTION_MOVE -> {
 //                val dx = event.x - downX
@@ -69,7 +69,7 @@ class ViewGroup2 : LinearLayout {
 //                Log.d(TAG, "onInterceptTouchEvent action:" + StringUtils.getMotionEventName(event) + " " + true);
 //                return true
 //            }
-        }
+//        }
         return super.onInterceptTouchEvent(event)
 //        Log.i(TAG, "onInterceptTouchEvent action:" + StringUtils.getMotionEventName(ev))
 //        val superReturn = super.onInterceptTouchEvent(ev)
@@ -81,6 +81,13 @@ class ViewGroup2 : LinearLayout {
         Log.i(TAG, "onTouchEvent          action:" + StringUtils.getMotionEventName(event))
 
         when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                downX = event.x
+                downY = event.y
+                isClicking = true
+                Log.d(TAG, "onTouchEvent action:" + StringUtils.getMotionEventName(event) + " " + true)
+                return true // 攔截 DOWN，防止子 View 消費
+            }
             MotionEvent.ACTION_MOVE -> {
                 val dx = event.x - downX
                 val dy = event.y - downY
